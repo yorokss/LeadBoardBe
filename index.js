f2b8630c3da8a07643fafa050862fa7a7c788cee
unknown <yorokss@gmail.com>
2025-02-17 15:29:09 +0530

    vercil

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const connectToDb = require("./db");
 connectToDb();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-//app.use(rateLimiter);
+app.use(rateLimiter);
 
 app.get("/health", (req, res) => {
   res.send("Server is running!");
