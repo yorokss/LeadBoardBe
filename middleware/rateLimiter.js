b8dd5a02e8f5f604b7745db2c25855f63f781b3f
unknown <yorokss@gmail.com>
2025-02-17 15:48:47 +0530

    rate limit increased

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -3,7 +3,7 @@ const rateLimiters = require("express-rate-limit");
 
 let rateLimiter = rateLimiters({
     windowMs: 15 * 60 * 1000, 
-    max: 5,
+    max: 50,
     message: { error: "Too many requests, please try again later." },
     standardHeaders: true, 
     legacyHeaders: false, 
